8639c74f8dfd8103df61f669149c74c4fc1dd618
root <root@girard.enc.sorbonne.fr>
2018-03-13 11:40:19 +0100

    fix

--- config.py
+++ config.py
@@ -4,12 +4,11 @@ import urllib.request
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    #local_filename, headers = urllib.request.urlretrieve(
-    #    'https://github.com/chartes/adele/raw/master/adele.sqlite',
-    #    os.path.join('db', 'adele.sqlite')
-    #)
+    local_filename, headers = urllib.request.urlretrieve(
+        'https://github.com/chartes/adele/raw/master/adele.sqlite',
+        os.path.join('db', 'adele.sqlite')
+    )
 
-    local_filename = '/var/lib/flask/adele-app/db/adele.sqlite'
 
     SQLALCHEMY_DATABASE_URI = local_filename
     SQLALCHEMY_TRACK_MODIFICATIONS = False
