59923d56288d44a9721a22ad94fb9f903e34932a
Pilla Julien <vrlitwhai.kridanik@gmail.com>
2018-03-20 11:12:41 +0100

    uncomment

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 
-    #try:
-    #    with urllib.request.urlopen('https://github.com/chartes/adele/raw/master/adele.sqlite') as response,\
-    #            open(os.path.join('db', 'adele.sqlite'), 'wb') as out_file:
-    #        shutil.copyfileobj(response, out_file)
-    #except:
-    #    pass
-    #    #local_filename="/Users/mrgecko/Documents/Dev/Data/adele/adele.sqlite"
+    try:
+        with urllib.request.urlopen('https://github.com/chartes/adele/raw/master/adele.sqlite') as response,\
+                open(os.path.join('db', 'adele.sqlite'), 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+    except:
+        pass
+        #local_filename="/Users/mrgecko/Documents/Dev/Data/adele/adele.sqlite"
 
     SQLALCHEMY_DATABASE_URI = os.path.join('db', 'adele.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
