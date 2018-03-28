68d4ca95dd98be3bfdb81dc51f7818f7ae78d118
Morgan Legal <morgan.legal@polenordstudio.fr>
2018-03-28 18:06:08 +0200

    change models

--- app/models.py
+++ app/models.py
@@ -5,58 +5,75 @@ from sqlalchemy.orm import relationship, backref
 
 
 association_document_has_act_type = Table('document_has_acte_type', Base.metadata,
-    Column('doc_id', Integer, ForeignKey('document.doc_id')),
-    Column('type_id', Text, ForeignKey('acte_type.type_id'))
+    Column('doc_id', Integer, ForeignKey('document.id')),
+    Column('type_id', Text, ForeignKey('acte_type.id'))
 )
 association_document_has_editor = Table('document_has_editor', Base.metadata,
-    Column('doc_id', Integer, ForeignKey('document.doc_id')),
-    Column('editor_name', Text, ForeignKey('editor.editor_name'))
+    Column('doc_id', Integer, ForeignKey('document.id')),
+    Column('editor_id', Text, ForeignKey('editor.id'))
 )
 association_document_has_language = Table('document_has_language', Base.metadata,
-    Column('doc_id', Integer, ForeignKey('document.doc_id')),
-    Column('lang_code', Text, ForeignKey('language.lang_code'))
+    Column('doc_id', Integer, ForeignKey('document.id')),
+    Column('lang_code', Text, ForeignKey('language.code'))
 )
 association_document_has_tradition = Table('document_has_tradition', Base.metadata,
-    Column('doc_id', Integer, ForeignKey('document.doc_id')),
-    Column('tradition_id', Text, ForeignKey('tradition.tradition_id'))
+    Column('doc_id', Integer, ForeignKey('document.id')),
+    Column('tradition_id', Text, ForeignKey('tradition.id'))
 )
 association_document_from_country = Table('document_from_country', Base.metadata,
-    Column('doc_id', Integer, ForeignKey('document.doc_id')),
-    Column('country_ref', Text, ForeignKey('country.country_ref'))
+    Column('doc_id', Integer, ForeignKey('document.id')),
+    Column('country_ref', Text, ForeignKey('country.ref'))
 )
 association_document_from_district = Table('document_from_district', Base.metadata,
-    Column('doc_id', Integer, ForeignKey('document.doc_id')),
-    Column('district_id', Integer, ForeignKey('district.district_id'))
+    Column('doc_id', Integer, ForeignKey('document.id')),
+    Column('district_id', Integer, ForeignKey('district.id'))
 )
 
 
 class ActType(Base):
     __tablename__ = 'acte_type'
-    type_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     label = Column(Text)
     description = Column(Text)
+    def serialize(self):
+        return {
+            'id': self.id,
+            'label': self.label,
+            'description': self.label
+        }
 
 class Country(Base):
     __tablename__ = 'country'
-    country_ref = Column(Text, primary_key=True)
-    country_label = Column(Text)
+    ref = Column(Text, primary_key=True)
+    label = Column(Text)
+    def serialize(self):
+        return {
+            'ref': self.ref,
+            'label': self.label
+        }
 
 class District(Base):
     __tablename__ = 'district'
-    district_id = Column(Integer, primary_key=True)
-    district_label = Column(Text)
+    id = Column(Integer, primary_key=True)
+    label = Column(Text)
     country_ref = Column(Text)
+    def serialize(self):
+        return {
+            'code': self.code,
+            'label': self.label,
+            'country_ref': self.country_ref
+        }
 
 class Document(Base):
     __tablename__ = 'document'
-    doc_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     title = Column(Text)
     subtitle = Column(Text)
     creation = Column(Integer)
     creation_lab = Column(Text)
     copy_year = Column(Text)
     copy_cent = Column(Text)
-    institution_ref = Column(Text, ForeignKey('institution.instit_ref'))
+    institution_ref = Column(Text, ForeignKey('institution.ref'))
     pressmark = Column(Text)
     argument = Column(Text)
     date_insert = Column(Text)
@@ -65,36 +82,72 @@ class Document(Base):
                                backref=backref('Document', lazy=True))
     act_types = relationship("ActType",
                              secondary=association_document_has_act_type,
-                             primaryjoin=(association_document_has_act_type.c.doc_id == doc_id))
+                             primaryjoin=(association_document_has_act_type.c.doc_id == id))
     countries = relationship("Country",
                             secondary=association_document_from_country)
     districts = relationship("District",
                              secondary=association_document_from_district,
-                             primaryjoin=(association_document_from_district.c.doc_id == doc_id))
+                             primaryjoin=(association_document_from_district.c.doc_id == id))
     editors = relationship("Editor",
                              secondary=association_document_has_editor)
     languages = relationship("Language",
                              secondary=association_document_has_language)
     traditions = relationship("Tradition",
                              secondary=association_document_has_tradition)
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'creation': self.creation,
+            'creation_lab': self.creation_lab,
+            'copy_year': self.copy_year,
+            'copy_cent': self.copy_cent,
+            'pressmark': self.pressmark,
+            'argument': self.argument,
+            'date_insert': self.pressmark,
+            'date_update': self.pressmark,
+            'institution': self.institution.serialize(),
+            'act_types': [at.serialize() for at in self.act_types]
+        }
 
 class Editor(Base):
     __tablename__ = 'editor'
-    editor_ref = Column(Text)
-    editor_name = Column(Text, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    ref = Column(Text)
+    name = Column(Text)
+    def serialize(self):
+        return {
+            'ref': self.ref,
+            'name': self.name
+        }
 
 class Institution(Base):
     __tablename__ = 'institution'
-    instit_ref = Column(Text, primary_key=True)
-    instit_name = Column(Text)
+    ref = Column(Text, primary_key=True)
+    name = Column(Text)
+    def serialize(self):
+        return {
+            'ref': self.ref,
+            'name': self.name
+        }
 
 class Language(Base):
     __tablename__ = 'language'
-    lang_code = Column(Text, primary_key=True)
-    lang_label = Column(Text)
+    code = Column(Text, primary_key=True)
+    label = Column(Text)
+    def serialize(self):
+        return {
+            'code': self.code,
+            'label': self.label
+        }
 
 class Tradition(Base):
     __tablename__ = 'tradition'
-    tradition_id = Column(Text, primary_key=True)
-    tradition_label = Column(Text)
+    id = Column(Text, primary_key=True)
+    label = Column(Text)
+    def serialize(self):
+        return {
+            'id': self.id,
+            'label': self.name
+        }
 
