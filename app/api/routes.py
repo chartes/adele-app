2c598d18abc05d7c02a998667be3fbf1f05f984c
Pilla Julien <vrlitwhai.kridanik@gmail.com>
2018-03-30 16:27:40 +0200

    move api routes to a new module

---
+++ app/api/routes.py
@@ -0,0 +1,72 @@
+from flask import jsonify
+from sqlalchemy.orm.exc import NoResultFound
+
+from app import app
+from app.api.response import APIResponseFactory
+from app.database.alignment.alignment_translation import align_translation
+from app.models import Image, User, Document
+
+
+"""
+---------------------------------
+API Routes
+---------------------------------
+"""
+
+@app.route('/api/<api_version>/alignment/translation/<transcription_id>/<translation_id>')
+def api_align_translation(api_version, transcription_id, translation_id):
+    alignment = align_translation(transcription_id, translation_id)
+    if len(alignment) > 0:
+        data = []
+        for al in alignment:
+            data.append({
+                "ptr_transcription_start": al[2],
+                "ptr_transcription_end": al[3],
+                "ptr_translation_start": al[4],
+                "ptr_translation_end": al[5],
+                "transcription": al[6],
+                "translation": al[7],
+            })
+        response = APIResponseFactory.make_response(data=data)
+    else:
+        response = APIResponseFactory.make_response(errors={"status": 404, "title": "Alignement introuvable"})
+    return jsonify(response)
+
+
+@app.route('/api/<api_version>/document/<doc_id>/manifest')
+def api_document_manifest(api_version, doc_id):
+    try:
+        img = Image.query.filter(Image.doc_id == doc_id).one()
+        response = APIResponseFactory.make_response(data={
+            "manifest_url": img.manifest_url
+        })
+    except NoResultFound:
+        response = APIResponseFactory.make_response(errors={
+            "status": 404, "title": "Manifeste introuvable pour le document {0}".format(doc_id)
+        })
+    return jsonify(response)
+
+@app.route('/api/<api_version>/document/<doc_id>')
+def api_document(api_version, doc_id):
+    try:
+        doc = Document.query.filter(Document.id == doc_id).one()
+        response = APIResponseFactory.make_response(data=doc.serialize())
+    except NoResultFound:
+        response = APIResponseFactory.make_response(errors={
+            "status": 404, "title": "Document {0} introuvable".format(doc_id)
+        })
+    return jsonify(response)
+
+#@app.route('/api/document/<doc_id>/traduction')
+#@app.route('/api/document/<doc_id>/transcription')
+
+@app.route('/api/<api_version>/user/<user_id>')
+def api_user(api_version, user_id):
+    try:
+        user = User.query.filter(User.id == user_id).one()
+        response = APIResponseFactory.make_response(data=user.serialize())
+    except NoResultFound:
+        response = APIResponseFactory.make_response(errors={
+            "status": 404, "title": "Utilisateur {0} introuvable".format(user_id)
+        })
+    return jsonify(response)
--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,14 @@
-from flask import render_template, flash, redirect, url_for, jsonify, render_template_string
+from flask import render_template, flash, redirect, url_for, render_template_string
 from flask_user import login_required
-from sqlalchemy.orm.exc import NoResultFound
 
 from app import app
-from app.api.response import APIResponseFactory
 from app.database.alignment.alignment_translation import align_translation
-from app.models import Document, User
-
+from app.models import Document, User, Image
 
 """
+---------------------------------
 Test routes
+---------------------------------
 """
 @app.route('/test/alignment/translation/<transcription_id>/<translation_id>')
 def r_align_translation(transcription_id, translation_id):
@@ -30,7 +29,7 @@ User Managment Routes
 
 @app.route('/members')
 @login_required
-def members_page():
+def r_members_page():
     return render_template_string("""
         {% extends "base.html" %}
         {% block content %}
@@ -78,54 +77,4 @@ def admin_document_edit(doc_id):
 API Routes
 ---------------------------------
 """
-
-@app.route('/api/<api_version>/alignment/translation/<transcription_id>/<translation_id>')
-def r_api_align_translation(api_version, transcription_id, translation_id):
-    alignment = align_translation(transcription_id, translation_id)
-    if len(alignment) > 0:
-        data = []
-        for al in alignment:
-            data.append({
-                "ptr_transcription_start": al[2],
-                "ptr_transcription_end": al[3],
-                "ptr_translation_start": al[4],
-                "ptr_translation_end": al[5],
-                "transcription": al[6],
-                "translation": al[7],
-            })
-        response = APIResponseFactory.make_response(data=data)
-    else:
-        response = APIResponseFactory.make_response(errors={"status": 404, "title": "Alignement introuvable"})
-
-    return jsonify(response)
-
-
-
-@app.route('/api/<api_version>/document/<doc_id>')
-def r_api_document(api_version, doc_id):
-    try:
-        doc = Document.query.filter(Document.id == doc_id).one()
-    except NoResultFound:
-        doc = None
-
-    if doc is None:
-        response = APIResponseFactory.make_response(errors={"status": 404, "title": "Document introuvable"})
-    else:
-        response = APIResponseFactory.make_response(data=doc.serialize())
-    return jsonify(response)
-
-#@app.route('/api/document/<doc_id>/traduction')
-#@app.route('/api/document/<doc_id>/transcription')
-
-@app.route('/api/<api_version>/user/<user_id>')
-def r_api_user(api_version, user_id):
-    try:
-        user = User.query.filter(User.id == user_id).one()
-    except NoResultFound:
-        user = None
-
-    if user is None:
-        response = APIResponseFactory.make_response(errors={"status": 404, "title": "Utilisateur introuvable"})
-    else:
-        response = APIResponseFactory.make_response(data=user.serialize())
-    return jsonify(response)
+from app.api.routes import *
