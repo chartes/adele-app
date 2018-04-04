b6e454ab2fc5966ddcd5707f57fd96efd7b0c983
Pilla Julien <vrlitwhai.kridanik@gmail.com>
2018-04-04 11:51:47 +0200

    gunicorn debug config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,5 +6,6 @@ workers = multiprocessing.cpu_count() * 2 + 1
 reload=True
 #accesslog='/var/log/flask/adele-app-access.log'
 #errorlog='/var/log/flask/adele-app-error.log'
-
+loglevel = 'debug'
 proc_name='adele'
+
