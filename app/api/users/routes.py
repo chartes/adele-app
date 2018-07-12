7c39ba93fc0b7f0b20a975918519ff595656c1fb
MrGecko <vrlitwhai.kridanik@gmail.com>
2018-07-12 16:52:00 +0200

    add whitelist features

--- app/api/users/routes.py
+++ app/api/users/routes.py
@@ -1,10 +1,11 @@
 from flask import request, current_app
+from sqlalchemy import func
 from sqlalchemy.orm.exc import NoResultFound
 
 from app import auth, db
 from app.api.response import APIResponseFactory
 from app.api.routes import api_bp
-from app.models import User, Role
+from app.models import User, Role, Whitelist
 
 """
 ===========================
@@ -214,4 +215,154 @@ def api_delete_users_roles(api_version, user_id):
             response = APIResponseFactory.make_response(errors={
                 "status": 404, "title": "User {0} not found".format(user_id)
             })
+    return APIResponseFactory.jsonify(response)
+
+
+@api_bp.route('/api/<api_version>/users/add-to-whitelist', methods=['POST'])
+@auth.login_required
+def add_user_to_whitelist(api_version):
+    """
+    {
+        data: {
+            emails : ['aa@bb.fr', 'aaabbbi@.fr'],
+            whitelist_id : 1
+        }
+    }
+    :return:
+    """
+    user = current_app.get_current_user()
+    response = None
+    if user.is_anonymous or (not user.is_teacher and not user.is_admin):
+        response = APIResponseFactory.make_response(errors={
+            "status": 403, "title": "Access forbidden"
+        })
+
+    if response is None:
+        data = request.get_json()
+        data = data["data"]
+        emails = data.get("emails") or []
+        whitelist_id = data.get("whitelist_id")
+
+        whitelist = Whitelist.query.filter(Whitelist.id == whitelist_id).first()
+        users = User.query.filter(User.email.in_(emails)).all()
+        # make sure you don't twice a same user
+        whitelist.users.extend([u for u in users if u.id not in [u.id for u in whitelist.users]])
+
+        response = APIResponseFactory.make_response(data={
+            "users": [u.serialize() for u in users],
+            "whitelist": whitelist.serialize()
+        })
+
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            response = APIResponseFactory.make_response(errors={
+                "status": 403, "title": "Cannot insert data", "details": str(e)
+            })
+
+    return APIResponseFactory.jsonify(response)
+
+
+@api_bp.route('/api/<api_version>/users/<user_id>/remove-from-whitelist/<whitelist_id>', methods=['DELETE'])
+@auth.login_required
+def remove_user_from_whitelist(api_version, user_id, whitelist_id):
+    """
+    :return:
+    """
+    user = current_app.get_current_user()
+    response = None
+    if user.is_anonymous or (not user.is_teacher and not user.is_admin):
+        response = APIResponseFactory.make_response(errors={
+            "status": 403, "title": "Access forbidden"
+        })
+
+    if response is None:
+
+        whitelist = Whitelist.query.filter(Whitelist.id == whitelist_id).first()
+        user = User.query.filter(User.id == user_id).first()
+
+        # remove the user from the whitelist
+        whitelist.users = [u for u in whitelist.users if u.id != int(user.id)]
+
+        response = APIResponseFactory.make_response(data=[])
+
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            response = APIResponseFactory.make_response(errors={
+                "status": 403, "title": "Cannot delete data", "details": str(e)
+            })
+
+    return APIResponseFactory.jsonify(response)
+
+
+@api_bp.route('/api/<api_version>/users/add-whitelist', methods=['POST'])
+@auth.login_required
+def add_whitelist(api_version):
+    """
+    {
+        data: {
+            name : 'My new whitelist'
+        }
+    }
+    :return:
+    """
+    user = current_app.get_current_user()
+    response = None
+    if user.is_anonymous or (not user.is_teacher and not user.is_admin):
+        response = APIResponseFactory.make_response(errors={
+            "status": 403, "title": "Access forbidden"
+        })
+
+    if response is None:
+        data = request.get_json()
+        data = data["data"]
+        new_whitelist_name = data.get("whitelist_name")
+
+        whitelist = Whitelist(label=new_whitelist_name)
+        db.session.add(whitelist)
+
+        try:
+            db.session.commit()
+            new_id = db.session.query(func.max(Whitelist.id))
+            new_whitelist = Whitelist.query.filter(Whitelist.id == new_id).first()
+            response = APIResponseFactory.make_response(data={
+                "whitelist": new_whitelist.serialize()
+            })
+        except Exception as e:
+            db.session.rollback()
+            response = APIResponseFactory.make_response(errors={
+                "status": 403, "title": "Cannot insert data", "details": str(e)
+            })
+
+    return APIResponseFactory.jsonify(response)
+
+
+@api_bp.route('/api/<api_version>/users/delete-whitelist/<whitelist_id>', methods=['DELETE'])
+@auth.login_required
+def delete_whitelist(api_version, whitelist_id):
+    """
+    :return:
+    """
+    user = current_app.get_current_user()
+    response = None
+    if user.is_anonymous or (not user.is_teacher and not user.is_admin):
+        response = APIResponseFactory.make_response(errors={
+            "status": 403, "title": "Access forbidden"
+        })
+
+    if response is None:
+        try:
+            whitelist = Whitelist.query.filter(Whitelist.id == whitelist_id).first()
+            db.session.remove(whitelist)
+            db.session.commit()
+            response = APIResponseFactory.make_response(data=[])
+        except Exception as e:
+            db.session.rollback()
+            response = APIResponseFactory.make_response(errors={
+                "status": 403, "title": "Cannot delete data", "details": str(e)
+            })
+
     return APIResponseFactory.jsonify(response)
