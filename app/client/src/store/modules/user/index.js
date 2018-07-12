6122b8af7098616cb1f300f5bd4a18a40d775057
Morgan Legal <morgan.legal@polenordstudio.fr>
2018-07-12 17:08:45 +0200

    change roles check

--- app/client/src/store/modules/user/index.js
+++ app/client/src/store/modules/user/index.js
@@ -2,62 +2,67 @@ import axios from "axios/index";
 
 const state = {
 
-  currentUser: undefined,
-  authToken: undefined,
-  author: undefined,
+    currentUser: undefined,
+    authToken: undefined,
+    author: undefined,
 
 };
 
 const mutations = {
 
-  UPDATE_CURRENT_USER (state, payload) {
-    state.currentUser = payload;
-  },
-  UPDATE_AUTH_TOKEN (state, payload) {
-    state.authToken = payload;
-  }
+    UPDATE_CURRENT_USER (state, payload) {
+        state.currentUser = payload;
+        state.author = payload;
+    },
+    UPDATE_AUTH_TOKEN (state, payload) {
+        state.authToken = payload;
+    }
 
 };
 
 const actions = {
 
-  getCurrentUser ({ commit }) {
-    return axios.get('/adele/api/1.0/user').then( (response) => {
-      commit('UPDATE_CURRENT_USER', response.data.data)
-    })
-  },
-  setAuthToken ({ commit }, token) {
-    commit('UPDATE_AUTH_TOKEN', token)
-  }
+    getCurrentUser ({ commit }) {
+        return axios.get('/adele/api/1.0/user').then( (response) => {
+            commit('UPDATE_CURRENT_USER', response.data.data)
+        })
+    },
+    setAuthToken ({ commit }, token) {
+        commit('UPDATE_AUTH_TOKEN', token)
+    },
+    setAuthor({ commit }, authorId) {
+
+    }
 
 };
 
 const checkRole = (user, role) => {
-  user && user.role === role
+  user && user.roles.indexOf(role) > -1;
 };
 
 const getters = {
 
-  authToken: state => state.authToken,
-  authHeader: state => { return { auth: { username: state.authToken, password: undefined }}},
-  currentUser: state => state.currentUser,
-  currentUserIsAdmin: state => {
-    return checkRole(state.currentUser, 'admin')
-  },
-  currentUserIsProfessor: state => {
-    return checkRole(state.currentUser, 'professor')
-  },
-  currentUserIsStudentstate: state => {
-    return checkRole(state.currentUser, 'student')
-  }
+    authToken: state => state.authToken,
+    authHeader: state => { return { auth: { username: state.authToken, password: undefined }}},
+    currentUser: state => state.currentUser,
+    currentUserIsAdmin: state => {
+        return checkRole(state.currentUser, 'admin')
+    },
+    currentUserIsTeacher: state => {
+        return checkRole(state.currentUser, 'teacher')
+    },
+    currentUserIsStudentstate: state => {
+        return checkRole(state.currentUser, 'student')
+    },
+
 };
 
 const userModule = {
-  namespaced: true,
-  state,
-  mutations,
-  actions,
-  getters
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+    getters
 }
 
 export default userModule;
