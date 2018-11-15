f176b4e355fb461b21ded0d30709468b7daba975
Morgan Legal <morgan.legal@polenordstudio.fr>
2018-11-15 13:05:35 +0100

    delete console.log in del blot

--- app/client/src/modules/quill/blots/semantic/Del.js
+++ app/client/src/modules/quill/blots/semantic/Del.js
@@ -10,8 +10,6 @@ import Quill from 'quill';
 
 let Inline = Quill.import('blots/inline');
 
-console.log("Inline.order", Inline.order)
-
 class Del extends Inline { }
 Del.blotName = 'del';
 Del.tagName = 'del';
