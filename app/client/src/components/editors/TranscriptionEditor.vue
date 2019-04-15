eddd7e40029d719b520542f311bd24c672889585
MrGecko <vrlitwhai.kridanik@gmail.com>
2019-04-15 11:07:14 +0200

    add ModalConfirmCloneTranscription

--- app/client/src/components/editors/TranscriptionEditor.vue
+++ app/client/src/components/editors/TranscriptionEditor.vue
@@ -42,7 +42,7 @@
                 :cancel="newNoteChoiceClose"
             />
         </div>
-
+        
         <notes-list-form
             v-if="noteEditMode == 'list'"
             :noteId="selectedNoteId"
@@ -57,94 +57,93 @@
             :submit="submitTextfieldForm"
             :cancel="cancelTextfieldForm"/>
         <note-form
-                v-if="noteEditMode == 'new' || noteEditMode == 'edit'"
-                :title="noteEditMode == 'new' ? 'nouvelle note' : 'Éditer la note'"
-                :note="currentNote"
-                :noteId="selectedNoteId"
-                :submit="updateNote"
-                :cancel="closeNoteEdit"
+            v-if="noteEditMode == 'new' || noteEditMode == 'edit'"
+            :title="noteEditMode == 'new' ? 'nouvelle note' : 'Éditer la note'"
+            :note="currentNote"
+            :noteId="selectedNoteId"
+            :submit="updateNote"
+            :cancel="closeNoteEdit"
         />
         <modal-confirm-note-delete
-                v-if="noteEditMode == 'delete'"
-                :cancel="closeNoteEdit"
-                :submit="deleteNote"
+            v-if="noteEditMode == 'delete'"
+            :cancel="closeNoteEdit"
+            :submit="deleteNote"
         />
-
+        
     </div>
 </template>
 
 <script>
-
-  import { mapState } from 'vuex'
-  import EditorButton from './EditorButton.vue';
-  import EditorMixins from '../../mixins/EditorMixins'
-  import EditorNotesMixins from '../../mixins/EditorNotesMixins'
-  import InEditorActions from './InEditorActions';
-  import NoteActions from './NoteActions';
-  import NewNoteActions from './NewNoteActions';
-  import NoteForm from '../forms/NoteForm';
-  import NotesListForm from '../forms/NotesListForm';
-  import ModalConfirmNoteDelete from '../forms/ModalConfirmNoteDelete';
-  import SaveBar from "../ui/SaveBar";
-  import TextfieldForm from "../forms/TextfieldForm";
-
-  export default {
-    name: "transcription-editor",
-    props: ['initialContent'],
-    mixins: [EditorMixins, EditorNotesMixins],
-    components: {
-      TextfieldForm,
-      SaveBar,
-      NewNoteActions,
-      InEditorActions,
-      EditorButton,
-      ModalConfirmNoteDelete,
-      NoteActions,
-      NotesListForm,
-      NoteForm,
-    },
-    data() {
-      return {
-        storeActions: {
-          changed: 'transcription/changed'
+    
+    import { mapState } from 'vuex'
+    import EditorButton from './EditorButton.vue';
+    import EditorMixins from '../../mixins/EditorMixins'
+    import EditorNotesMixins from '../../mixins/EditorNotesMixins'
+    import InEditorActions from './InEditorActions';
+    import NoteActions from './NoteActions';
+    import NewNoteActions from './NewNoteActions';
+    import NoteForm from '../forms/NoteForm';
+    import NotesListForm from '../forms/NotesListForm';
+    import ModalConfirmNoteDelete from '../forms/ModalConfirmNoteDelete';
+    import SaveBar from "../ui/SaveBar";
+    import TextfieldForm from "../forms/TextfieldForm";
+    
+    export default {
+        name: "transcription-editor",
+        props: ['initialContent'],
+        mixins: [EditorMixins, EditorNotesMixins],
+        components: {
+            TextfieldForm,
+            SaveBar,
+            NewNoteActions,
+            InEditorActions,
+            EditorButton,
+            ModalConfirmNoteDelete,
+            NoteActions,
+            NotesListForm,
+            NoteForm,
         },
-        delta: null,
-        buttons: {
-          bold: false,
-          italic: false,
-          superscript: false,
-          smallcaps: false,
-          underline: false,
-          del: false,
-          expan: false,
-          person: false,
-          location: false,
-          note: false,
-          segment: false,
-          paragraph: false,
+        data() {
+            return {
+                storeActions: {
+                    changed: 'transcription/changed'
+                },
+                delta: null,
+                buttons: {
+                    bold: false,
+                    italic: false,
+                    superscript: false,
+                    smallcaps: false,
+                    underline: false,
+                    del: false,
+                    expan: false,
+                    person: false,
+                    location: false,
+                    note: false,
+                    segment: false,
+                    paragraph: false,
+                }
+            }
+        },
+        mounted () {
+            
+            this.initEditor(this.$refs.editor, this.$props.initialContent);
+            
+        },
+        beforeDestroy () {
+            this.deactivateEvents();
+        },
+        methods: {
+            
+            updateContent () {
+                this.delta = this.editor.getContents().ops;
+                
+            }
+        },
+        
+        computed: {
+            ...mapState('transcription', ['transcriptionSaved']),
+            ...mapState('translation', ['translationSaved']),
         }
-      }
-    },
-    mounted () {
-
-      this.initEditor(this.$refs.editor, this.$props.initialContent);
-
-    },
-    beforeDestroy () {
-      this.deactivateEvents();
-    },
-    methods: {
-
-      updateContent () {
-        this.delta = this.editor.getContents().ops;
-
-      },
-
-    },
-
-    computed: {
-      ...mapState('transcription', ['transcriptionSaved']),
-      ...mapState('translation', ['translationSaved']),
     }
-  }
 </script>
