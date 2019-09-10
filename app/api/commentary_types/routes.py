0346f219eb37cceaae69b969fc3fefbc4f0b07ce
Julien Pilla <vrlitwhai.kridanik@gmail.com>
2019-09-10 10:48:29 +0200

    add tests for commentary types  API side

--- app/api/commentary_types/routes.py
+++ app/api/commentary_types/routes.py
@@ -1,167 +1,118 @@
-from flask import request, url_for, current_app
+from flask import request, current_app
 from sqlalchemy.orm.exc import NoResultFound
 
-from app import APIResponseFactory, db, auth
-from app.api.routes import api_bp, query_json_endpoint
+from app import db, auth
+from app.api.routes import api_bp, json_loads
 from app.models import CommentaryType
+from app.utils import forbid_if_nor_teacher_nor_admin, make_404, make_200, make_409
 
 
 @api_bp.route('/api/<api_version>/commentary-types')
 @api_bp.route('/api/<api_version>/commentary-types/<commentary_type_id>')
 def api_commentary_type(api_version, commentary_type_id=None):
-    try:
-        if commentary_type_id is not None:
-            commentary_types = [CommentaryType.query.filter(CommentaryType.id == commentary_type_id).one()]
+    if commentary_type_id is None:
+        commentary_types = CommentaryType.query.all()
+    else:
+        # single
+        at = CommentaryType.query.filter(CommentaryType.id == commentary_type_id).first()
+        if at is None:
+            return make_404("CommentaryType {0} not found".format(commentary_type_id))
         else:
-            commentary_types = CommentaryType.query.all()
-        response = APIResponseFactory.make_response(data=[a.serialize() for a in commentary_types])
-    except NoResultFound:
-        response = APIResponseFactory.make_response(errors={
-            "status": 404, "title": "CommentaryType {0} not found".format(commentary_type_id)
-        })
-    return APIResponseFactory.jsonify(response)
+            commentary_types = [at]
+    return make_200([a.serialize() for a in commentary_types])
 
 
 @api_bp.route('/api/<api_version>/commentary-types', methods=['DELETE'])
 @api_bp.route('/api/<api_version>/commentary-types/<commentary_type_id>', methods=['DELETE'])
 @auth.login_required
 def api_delete_commentary_type(api_version, commentary_type_id=None):
-    response = None
-    user = current_app.get_current_user()
-    if user.is_anonymous or not (user.is_teacher or user.is_admin):
-        response = APIResponseFactory.make_response(errors={
-            "status": 403, "title": "Access forbidden"
-        })
-    if response is None:
-        try:
-            if commentary_type_id is not None:
-                commentary_types = [CommentaryType.query.filter(CommentaryType.id == commentary_type_id).one()]
-            else:
-                commentary_types = CommentaryType.query.all()
+    access_is_forbidden = forbid_if_nor_teacher_nor_admin(current_app)
+    if access_is_forbidden:
+        return access_is_forbidden
 
-            for c in commentary_types:
-                db.session.delete(c)
-            try:
-                db.session.commit()
-                response = APIResponseFactory.make_response(data=[])
-            except Exception as e:
-                db.session.rollback()
-                response = APIResponseFactory.make_response(errors={
-                    "status": 403, "title": "Cannot delete data", "details": str(e)
-                })
+    if commentary_type_id is None:
+        commentary_types = CommentaryType.query.all()
+    else:
+        commentary_types = CommentaryType.query.filter(CommentaryType.id == commentary_type_id).all()
 
-        except NoResultFound:
-            response = APIResponseFactory.make_response(errors={
-                "status": 404, "title": "CommentaryType {0} not found".format(commentary_type_id)
-            })
-    return APIResponseFactory.jsonify(response)
+    for a in commentary_types:
+        db.session.delete(a)
+    try:
+        db.session.commit()
+        return make_200([])
+    except Exception as e:
+        db.session.rollback()
+        return make_409(str(e))
 
 
 @api_bp.route('/api/<api_version>/commentary-types', methods=['PUT'])
 @auth.login_required
 def api_put_commentary_type(api_version):
-    response = None
-    user = current_app.get_current_user()
-    if user.is_anonymous or not (user.is_teacher or user.is_admin):
-        response = APIResponseFactory.make_response(errors={
-            "status": 403, "title": "Access forbidden"
-        })
-    if response is None:
-        try:
-            data = request.get_json()
+    access_is_forbidden = forbid_if_nor_teacher_nor_admin(current_app)
+    if access_is_forbidden:
+        return access_is_forbidden
 
-            if "data" in data:
-                data = data["data"]
+    try:
+        data = request.get_json()
 
-                if not isinstance(data, list):
-                    data = [data]
+        if "data" in data:
+            data = data["data"]
 
+            try:
                 modifed_data = []
-                try:
-                    for commentary_type in data:
-                        if "id" not in commentary_type:
-                            raise Exception("CommentaryType id is missing from the payload")
-                        a = CommentaryType.query.filter(CommentaryType.id == commentary_type["id"]).one()
-                        if "label" in commentary_type:
-                            a.label = commentary_type["label"]
-                        db.session.add(a)
-                        modifed_data.append(a)
+                for commentary_type in data:
+                    a = CommentaryType.query.filter(CommentaryType.id == commentary_type["id"]).one()
+                    a.label = commentary_type.get("label")
+                    a.description = commentary_type.get("description")
 
-                    db.session.commit()
-                except Exception as e:
-                    db.session.rollback()
-                    response = APIResponseFactory.make_response(errors={
-                        "status": 403, "title": "Cannot update data", "details": str(e)
-                    })
+                    db.session.add(a)
+                    modifed_data.append(a)
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                return make_409(str(e))
 
-                if response is None:
-                    data = []
-                    for a in modifed_data:
-                        json_obj = query_json_endpoint(
-                            request,
-                            url_for("api_bp.api_commentary_type", api_version=api_version, commentary_type_id=a.id)
-                        )
-                        print(json_obj)
-                        data.append(json_obj["data"])
-                    response = APIResponseFactory.make_response(data=data)
+            data = []
+            for a in modifed_data:
+                r = api_commentary_type(api_version=api_version, commentary_type_id=a.id)
+                data.append(json_loads(r.data)["data"])
 
-        except NoResultFound:
-            response = APIResponseFactory.make_response(errors={
-                "status": 404, "title": "CommentaryType not found"
-            })
-
-    return APIResponseFactory.jsonify(response)
+            return make_200(data)
+        else:
+            return make_409("no data")
+    except NoResultFound:
+        return make_404("CommentaryType not found")
 
 
 @api_bp.route('/api/<api_version>/commentary-types', methods=['POST'])
 @auth.login_required
 def api_post_commentary_type(api_version):
-    response = None
-    user = current_app.get_current_user()
-    if user.is_anonymous or not (user.is_teacher or user.is_admin):
-        response = APIResponseFactory.make_response(errors={
-            "status": 403, "title": "Access forbidden"
-        })
-    if response is None:
+    access_is_forbidden = forbid_if_nor_teacher_nor_admin(current_app)
+    if access_is_forbidden:
+        return access_is_forbidden
+
+    data = request.get_json()
+
+    if "data" in data:
+        data = data["data"]
+
+        created_data = []
         try:
-            data = request.get_json()
+            for commentary_type in data:
+                a = CommentaryType(**commentary_type)
+                db.session.add(a)
+                created_data.append(a)
 
-            if "data" in data:
-                data = data["data"]
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return make_409(str(e))
 
-                if not isinstance(data, list):
-                    data = [data]
+        data = []
+        for a in created_data:
+            r = api_commentary_type(api_version=api_version, commentary_type_id=a.id)
+            data.append(json_loads(r.data)["data"])
 
-                created_data = []
-                for commentary_type in data:
-                    if "id" in commentary_type:
-                        commentary_type.pop("id")
-                    a = CommentaryType(**commentary_type)
-                    db.session.add(a)
-                    created_data.append(a)
-
-                try:
-                    db.session.commit()
-                except Exception as e:
-                    db.session.rollback()
-                    response = APIResponseFactory.make_response(errors={
-                        "status": 403, "title": "Cannot insert data", "details": str(e)
-                    })
-
-                if response is None:
-                    data = []
-                    for a in created_data:
-                        json_obj = query_json_endpoint(
-                            request,
-                            url_for("api_bp.api_commentary_type", api_version=api_version, commentary_type_id=a.id)
-                        )
-                        data.append(json_obj["data"])
-                    response = APIResponseFactory.make_response(data=data)
-
-        except NoResultFound:
-            response = APIResponseFactory.make_response(errors={
-                "status": 404, "title": "CommentaryType not found"
-            })
-
-    return APIResponseFactory.jsonify(response)
-
+        return make_200(data)
+    else:
+        return make_409("no data")
---
+++ tests/api/test_commentary_types.py
@@ -0,0 +1,92 @@
+from os.path import join
+from tests.base_server import TestBaseServer, json_loads, ADMIN_USER, STU1_USER, PROF1_USER, PROF2_USER
+
+
+class TestCommentaryTypesAPI(TestBaseServer):
+
+    def test_get_commentary_types(self):
+        self.assert404("/adele/api/1.0/commentary-types/100")
+
+        r = self.assert200('/adele/api/1.0/commentary-types')
+        r = json_loads(r.data)["data"]
+        self.assertEqual(6, len(r))
+
+        r = self.assert200('/adele/api/1.0/commentary-types/2')
+        r = json_loads(r.data)["data"]
+        self.assertEqual(2, r[0]["id"])
+
+    def test_delete_commentary_types(self):
+        self.assert403("/adele/api/1.0/commentary-types", method="DELETE")
+        self.assert403("/adele/api/1.0/commentary-types", method="DELETE", **STU1_USER)
+
+        self.delete_with_auth("/adele/api/1.0/commentary-types/2", **ADMIN_USER)
+        self.assert404("/adele/api/1.0/commentary-types/2")
+
+        r = self.assert200('/adele/api/1.0/commentary-types')
+        r = json_loads(r.data)["data"]
+        self.assertEqual(5, len(r))
+
+    def test_put_commentary_types(self):
+        self.assert403("/adele/api/1.0/commentary-types", data={"data": [{}]},  method="PUT")
+        self.assert403("/adele/api/1.0/commentary-types", data={"data": [{}]},  method="PUT", **STU1_USER)
+        self.assert409("/adele/api/1.0/commentary-types", data={"data": [{"id": 100}]},  method="PUT", **ADMIN_USER)
+
+        self.assert200("/adele/api/1.0/commentary-types",
+                       data={"data": [{"id": 2, "label": "PapeTest"}]},
+                       method="PUT", **ADMIN_USER)
+        r = self.assert200('/adele/api/1.0/commentary-types/2')
+        r = json_loads(r.data)["data"]
+        self.assertEqual("PapeTest", r[0]["label"])
+
+        self.assert200("/adele/api/1.0/commentary-types",
+                       data={"data": [{"id": 2, "label": "Desc2"}]},
+                       method="PUT", **ADMIN_USER)
+        r = self.assert200('/adele/api/1.0/commentary-types/2')
+        r = json_loads(r.data)["data"]
+        self.assertEqual("Desc2", r[0]["label"])
+
+        # put conflicting data
+        self.assert409("/adele/api/1.0/commentary-types",
+                       data={"data": [{"id": -1, "label": "ACTE-TYPT-500"}]},
+                       method="PUT", **ADMIN_USER)
+        self.assert409("/adele/api/1.0/commentary-types",
+                       data={"data": [{"id": 19, "label": "ACTE-TYPT-501"},
+                                      {"id": 500, "label": "ACTE-TYPT-502"}]},
+                       method="PUT", **PROF1_USER)
+
+        # put multiple data
+        self.assert200("/adele/api/1.0/commentary-types",
+                       data={"data": [{"id": 500, "label": "ACTE-TYPT-500"}]},
+                       method="POST", **ADMIN_USER)
+        self.assert200("/adele/api/1.0/commentary-types",
+                       data={"data": [{"id": 19, "label": "ACTE-TYPT-19"}]},
+                       method="POST", **ADMIN_USER)
+        self.assert200("/adele/api/1.0/commentary-types",
+                       data={"data": [{"id": 19, "label": "ACTE-TYPT-19"},
+                                      {"id": 500, "label": "ACTE-TYPT-5XX"}]},
+                       method="PUT", **PROF1_USER)
+
+    def test_api_post_commentary_type(self):
+        self.assert403("/adele/api/1.0/commentary-types", data={"data": [{}]},  method="POST")
+        self.assert403("/adele/api/1.0/commentary-types", data={"data": [{}]},  method="POST", **STU1_USER)
+        self.assert409("/adele/api/1.0/commentary-types", data={"data": [{"champ bidon": 100}]}, method="POST", **ADMIN_USER)
+
+        self.assert200("/adele/api/1.0/commentary-types",
+                       data={"data": [{"id": 500, "label": "ACTE-TYPT-500"}]}, method="POST", **ADMIN_USER)
+
+        r = self.assert200('/adele/api/1.0/commentary-types/500')
+        r = json_loads(r.data)["data"]
+        self.assertEqual(500, r[0]["id"])
+
+        # post conflicting data
+        self.assert409("/adele/api/1.0/commentary-types",
+                       data={"data": [{"id": 500, "label": "ACTE-TYPT-500"}]}, method="POST",
+                       **ADMIN_USER)
+
+        # post multiple data
+        self.assert200("/adele/api/1.0/commentary-types",
+                       data={"data": [{"id": 501, "label": "ACTE-TYPT-501"},
+                                      {"id": 502, "label": "ACTE-TYPT-502"}]}, method="POST",
+                       **PROF1_USER)
+        self.assert200('/adele/api/1.0/commentary-types/501')
+        self.assert200('/adele/api/1.0/commentary-types/502')
