46b5e88e006931cf0a7c8058795adff1744fff2e
Frank Bessou <frank.bessou@logilab.fr>
2022-01-27 15:47:52 +0100

    feat: allow users to reset their password
    There are two new API endpoints:
    - send-password-reset-link: accepts an email adress and sends a reset
    link
    - reset-password: accepts a valid jwt associated to a user and a
      password and update the user's password accordingly

--- app/api/auth.py
+++ app/api/auth.py
@@ -10,6 +10,7 @@ from flask_jwt_extended import (
 )
 from flask_mail import Message
 import jwt
+import jwt.exceptions
 from sqlalchemy import or_
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -17,6 +18,7 @@ from app import api_bp, make_403, db, mail
 from app.models import User, Role
 
 from app.utils import make_401, forbid_if_nor_teacher_nor_admin, make_400, make_200
+from .. import config
 
 
 @api_bp.route('/api/<api_version>/logout')
@@ -149,3 +151,66 @@ def update_user(api_version):
     resp = jsonify(resp)
 
     return resp, 200
+
+
+@api_bp.route('/api/<api_version>/send-password-reset-link', methods=['POST'])
+def send_password_reset_link(api_version):
+    json = request.get_json(force=True)
+    email = json.get('email', None)
+    user = User.query.filter(User.email == email).first()
+
+    response = jsonify({"error": None})
+
+    if user is None:
+        print("User unknown")
+        # Don't leak existing users to client
+        return response, 200
+
+    print("Sending password reset link to ", user)
+
+    token = jwt.encode({
+        'sub': user.email,
+        'iat': datetime.datetime.utcnow(),
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=20)},
+        current_app.config['SECRET_KEY']).decode('ascii')
+    link = current_app.with_url_prefix('/reset-password?token=%s' % token)
+
+    msg = Message(
+        'Adele - Demande de récupération de mot de passe',
+        sender=current_app.config['MAIL_USERNAME'],
+        recipients=[email])
+    msg.body = "Vous avez demandé à réinitialiser votre mot de passe. "\
+               "Vous pouvez choisir un nouveau mot de passe en suivant ce lien: %s " \
+               "\nSi vous n'avez pas fait cette demande vous pouvez ignorer cet email." % link
+    mail.send(msg)
+    return response, 200
+
+
+@api_bp.route('/api/<api_version>/reset-password', methods=['POST'])
+def reset_password(api_version):
+    json = request.get_json(force=True)
+    password = json.get('password', None)
+    password2 = json.get('password2', None)
+
+    if password != password2:
+        print("Passwords do not match")
+        return jsonify({"error": "Les mots de passe ne sont pas identiques."}), 422
+
+    token = json.get('token', None)
+    try:
+        email = jwt.decode(token, key=current_app.config['SECRET_KEY'])['sub']
+    except jwt.exceptions.ExpiredSignatureError:
+        return jsonify({"error": "Ce lien est expiré, veuillez refaire une demande."}), 401
+    except jwt.exceptions.DecodeError:
+        return jsonify({
+            "error": "Ce lien n'est pas valide, essayez de copier/coller le"\
+                     "lien que vous avez reçu par mail dans la barre d'URL ou de refaire une demande."
+        }), 422
+
+    user = User.query.filter(User.email == email).first()
+    user.password = generate_password_hash(password)
+    db.session.add(user)
+    db.session.commit()
+
+    response = jsonify({"error": None})
+    return response, 200
